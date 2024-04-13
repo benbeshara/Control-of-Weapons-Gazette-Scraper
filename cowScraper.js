05ff67a3ca44833b67c0770b75a56446b2d56615
Ben Beshara <code@benbeshara.id.au>
2024-04-13 18:50:09 +1000

    Basic web serving of pdf links

---
+++ cowScraper.js
@@ -0,0 +1,83 @@
+import { load } from "cheerio";
+import { get } from "node:http";
+import { ParsePDF } from "./parsePDF.js";
+import fs from "node:fs/promises";
+
+const baseurl = "http://www.gazette.vic.gov.au";
+const url = "http://www.gazette.vic.gov.au/gazette_bin/gazette_archives.cfm";
+
+const cache = "cache.json";
+
+const fetchPdf = async (uri) => {
+  return new Promise((resolve, reject) => {
+    try {
+      ParsePDF(uri).then(async (data) => {
+        let row = { uri: uri, flagged: false };
+
+        // This seems to be the consistent string to look for in the PDFs
+        if (data.includes("Control of Weapons Act 1990")) {
+          row.flagged = true;
+        }
+        resolve(row);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const updatePdfs = async () => {
+  let jsn;
+
+  try {
+    jsn = await fs.readFile(cache).then((data) => JSON.parse(data));
+  } catch (err) {
+    console.error(err);
+    jsn = [];
+    await fs.writeFile(cache, JSON.stringify(jsn));
+  }
+
+  get(url, (res) => {
+    let pageData;
+    res.on("data", (chunk) => (pageData += chunk));
+    res.on("end", async () => {
+      let $ = load(pageData);
+      let gl = $("#special_gazettes a");
+
+      let pdfs = await Promise.allSettled(
+        gl.map(async (_, el) => {
+          return new Promise((resolve, reject) => {
+            let newuri = baseurl + $(el).attr("href");
+            for (let i of jsn) {
+              if (i["uri"] === newuri) {
+                reject();
+              }
+            }
+            resolve(newuri);
+          });
+        }),
+      );
+
+      pdfs = pdfs.filter((pdf) => pdf.status === "fulfilled");
+
+      await Promise.all(pdfs.map(async (uri) => fetchPdf(uri.value))).then(
+        (data) => {
+          data = jsn.concat(data);
+          fs.writeFile(cache, JSON.stringify(data));
+        },
+      );
+    });
+  });
+};
+
+export const listPdfs = async () => {
+  try {
+    await updatePdfs();
+    let ret = JSON.parse(await fs.readFile(cache));
+    ret = ret.filter((r) => r.flagged === true);
+    return ret;
+  } catch (e) {
+    console.error("Failed to read PDF list: ", e);
+    return;
+  }
+};
--- index.js
+++ index.js
@@ -1,75 +1,19 @@
-import { load } from "cheerio";
-import { get } from "node:http";
-import { ParsePDF } from "./parsePDF.js";
-import fs from "node:fs/promises";
+import { listPdfs } from "./cowScraper.js";
+import express from "express";
 
-const baseurl = "http://www.gazette.vic.gov.au";
-const url = "http://www.gazette.vic.gov.au/gazette_bin/gazette_archives.cfm";
+const app = express();
 
-const cache = "cache.json";
+console.info(await listPdfs());
 
-const fetchPdf = async (uri) => {
-  return new Promise((resolve, reject) => {
-    try {
-      ParsePDF(uri).then(async (data) => {
-        let row = { uri: uri, flagged: false };
+let pdfs = await listPdfs();
 
-        // This seems to be the consistent string to look for in the PDFs
-        if (data.includes("Control of Weapons Act 1990")) {
-          row.flagged = true;
-        }
-        resolve(row);
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
-const updatePdfs = async () => {
-  let jsn;
-
-  try {
-    jsn = await fs.readFile(cache).then((data) => JSON.parse(data));
-  } catch (err) {
-    console.error(err);
-    jsn = [];
-    await fs.writeFile(cache, JSON.stringify(jsn));
+app.get("/", (req, res) => {
+  let response = `<html><head><title>Control of Weapons Acts</title></head><body>`;
+  for (let x of pdfs) {
+    response += `<a href=${x["uri"]}>${x["uri"]}</a><br />`;
   }
+  response += `</body></html>`;
+  res.send(response);
+});
 
-  get(url, (res) => {
-    let pageData;
-    res.on("data", (chunk) => (pageData += chunk));
-    res.on("end", async () => {
-      // let pdfs = [];
-      let $ = load(pageData);
-      let gl = $("#special_gazettes a");
-
-      let pdfs = await Promise.allSettled(
-        gl.map(async (_, el) => {
-          return new Promise((resolve, reject) => {
-            let newuri = baseurl + $(el).attr("href");
-            for (let i of jsn) {
-              if (i["uri"] === newuri) {
-                reject();
-              }
-            }
-            resolve(newuri);
-          });
-        }),
-      );
-
-      pdfs = pdfs.filter((pdf) => pdf.status === "fulfilled");
-
-      await Promise.all(pdfs.map(async (uri) => fetchPdf(uri.value))).then(
-        (data) => {
-          console.log(data);
-          data = jsn.concat(data);
-          fs.writeFile(cache, JSON.stringify(data));
-        },
-      );
-    });
-  });
-};
-
-updatePdfs();
+app.listen(3000);
