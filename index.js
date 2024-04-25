23958d6c7a9d394a7a12f7bb78ea8176f419fb0b
bb <bb@benbeshara.id.au>
2024-04-26 00:48:09 +1000

    Lil of the ol' razzle dazzle

--- index.js
+++ index.js
@@ -11,12 +11,51 @@ app.get("/", (req, res) => {
     <script src="https://unpkg.com/htmx.org@1.9.12"></script>
     </head>
     <body>
-    <div>
-    <div hx-get="/list"
+    <div class="center">
+    <span class="heading">Control of Weapons Act Notices</span>
+    <span class="subheading">Gazettes sourced from the Victorian Gazette website</span>
+    <ul>
+    <li hx-get="/list"
         hx-trigger="load"
         hx-swap="outerHTML"
-    >Loading...</div>
+    >Loading (If there's a lot of new gazettes, this could take some time)</li>
+    </ul>
     </div>
+    <style>
+    body {
+      background-color: #225;
+      color: #CCC;
+      font-family: sans-serif;
+    }
+    div.center {
+      margin: auto;
+      width: 60%;
+    }
+    span.heading {
+      font-size: 1.8rem;
+      display: block;
+    }
+    span.subheading {
+      font-size: 1.4rem;
+      display: block;
+    }
+    span.uri {
+      font-size: 0.9rem;
+      color: #aaa;
+    }
+    a {
+      color: #CCC;
+      text-decoration: none;
+      font-size: 1.2rem;
+    }
+    ul {
+      width: 80%;
+      list-style-type: none
+    }
+    li {
+      padding: 0.5em 0px;
+    }
+    </style>
     </body>
     </html>`;
   res.send(response);
@@ -26,7 +65,7 @@ app.get("/list", async (req, res) => {
   let pdfs = await listPdfs();
   let response = "";
   for (let x of pdfs) {
-    response += `<a href=${x["uri"]}>${x["title"]}</a><br />`;
+    response += `<li><a href=${x["uri"]}>${x["title"]}<br /><span class="uri">${x["uri"]}</span></a></li>`;
   }
   res.send(response);
 });
