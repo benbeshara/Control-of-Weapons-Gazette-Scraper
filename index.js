39877effa52b47d1f09984c1eee387bde9333450
Ben Beshara <code@benbeshara.id.au>
2024-04-27 18:07:39 +1000

    Use SSEs to try to get around Heroku's timeout

--- index.js
+++ index.js
@@ -8,17 +8,21 @@ app.get("/", (req, res) => {
     <html>
     <head>
     <title>Control of Weapons Acts</title>
-    <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+    <script src="https://unpkg.com/htmx.org@2.0.0-beta3"></script>
+    <script src="https://unpkg.com/htmx-ext-sse@2.1.0/sse.js"></script>
     </head>
     <body>
     <div class="center">
     <span class="heading">Control of Weapons Act Notices</span>
     <span class="subheading">Gazettes sourced from the Victorian Gazette website</span>
     <ul>
-    <li hx-get="/list"
-        hx-trigger="load"
-        hx-swap="outerHTML"
-    >Loading (If there's a lot of new gazettes, this could take some time)</li>
+    <li hx-ext="sse"
+        sse-connect="/listSSE"
+        sse-close="close"
+        sse-swap="list"
+        hx-swap="outerHTML">
+      Loading (If there's a lot of new gazettes, this could take some time)
+    </li>
     </ul>
     </div>
     <style>
@@ -61,8 +65,23 @@ app.get("/", (req, res) => {
   res.send(response);
 });
 
+app.get("/listSSE", async (req, res) => {
+  const clientId = Date.now();
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+  res.writeHead(200, headers);
+  const newClient = {
+    id: clientId,
+    res,
+  };
+  updatePdfsInBackground(newClient);
+});
+
 app.get("/list", async (req, res) => {
-  let pdfs = await listPdfs();
+  const pdfs = await listPdfs();
   pdfs.sort((a, b) => {
     a = a.uri.toUpperCase();
     b = b.uri.toUpperCase();
@@ -85,7 +104,7 @@ app.get("/list", async (req, res) => {
 });
 
 app.get("/latest", async (req, res) => {
-  let pdfs = await listPdfs();
+  const pdfs = await listPdfs();
   pdfs.sort((a, b) => {
     a = a.uri.toUpperCase();
     b = b.uri.toUpperCase();
@@ -101,6 +120,35 @@ app.get("/latest", async (req, res) => {
   });
   res.send(pdfs);
 });
+
+const updatePdfsInBackground = (client) => {
+  listPdfs().then((pdfs) => {
+    pdfs.sort((a, b) => {
+      a = a.uri.toUpperCase();
+      b = b.uri.toUpperCase();
+
+      if (a < b) {
+        return 1;
+      }
+      if (a > b) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    let response = "";
+    for (let x of pdfs) {
+      response += `<li><a href=${x["uri"]}>${x["title"]}<br /><span class="uri">${x["uri"]}</span></a></li>`;
+    }
+
+    client.res.write("event: list\n");
+    client.res.write(`data: ${response}\n\n`);
+    client.res.write("event: close\n");
+    client.res.write("data: true\n");
+    client.res.end();
+  });
+};
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
